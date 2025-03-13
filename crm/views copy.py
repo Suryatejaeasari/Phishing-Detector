8f34cbacc39f49f65df34882dc985ca26b89cbbe
Suryatejaeasari <suryatejaeasari@gmail.com>
2025-03-13 20:44:27 +0530

    Initial commit

---
+++ crm/views copy.py
@@ -0,0 +1,336 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from tensorflow import keras 
+import re
+from urllib.parse import urlparse
+
+
+# First Directory Length
+def fd_length(url):
+    urlpath = urlparse(url).path
+    try:
+        return len(urlpath.split('/')[1])
+    except:
+        return 0
+
+def fd_length(url):
+    urlpath = urlparse(url).path
+    try:
+        return len(urlpath.split('/')[1])
+    except:
+        return 0
+
+def digit_count(url):
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits+1
+    return digits
+
+def digit_count(url):
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits + 1
+    return digits
+
+def digit_count(url):
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits + 1
+    return digits
+
+def letter_count(url):
+    letters = 0
+    for i in  url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+
+def letter_count(url):
+    letters = 0
+    for i in url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+
+def letter_count(url):
+    letters = 0
+    for i in url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+def no_of_dir(url):
+    urldir = urlparse(url).path
+    return urldir.count('/')
+
+def no_of_dir(url):
+    urldir = urlparse(url).path
+    return urldir.count('/')
+
+def no_of_dir(url):
+    urldir = urlparse(url).path
+    return urldir.count('/')
+
+def having_ip_address(url):
+    match = re.search(,url)
+    if match:
+        return -1
+    else:
+        return 1
+
+# Use of IP or not in domain
+def having_ip_address(url):
+    match = re.search(
+        # IPv4 in hexadecimal
+        '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
+        '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  # IPv4
+        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)'
+        '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # Ipv6
+    if match:
+        # print match.group()
+        return -1
+    else:
+        # print 'No matching pattern found'
+        return 1
+
+def having_ip_address(url):
+    match = re.search(,url)
+    if match:
+        return -1
+    else:
+        return 1
+    
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+def url_length(url):
+    return len(urlparse(url).path)
+
+def url_length(url):
+    return len(urlparse(url).path)
+
+def url_length(url):
+    return len(urlparse(url).path)
+
+def get_counts(url):
+    count_features = []
+    i = url.count('-')
+    count_features.append(i)
+    i = url.count('@')
+    count_features.append(i)
+    i = url.count('?')
+    count_features.append(i)
+    i = url.count('%')
+    count_features.append(i)
+    i = url.count('.')
+    count_features.append(i)
+    i = url.count('=')
+    count_features.append(i)
+    i = url.count('http')
+    count_features.append(i)
+    i = url..count('https')
+    count_features.append(i)
+    i = url.count('www')
+    count_features.append(i)
+    
+    return count_features
+
+
+# Gets all count features
+def get_counts(url):
+
+    count_features = []
+
+    i = url.count('-')
+    count_features.append(i)
+
+    i = url.count('@')
+    count_features.append(i)
+
+    i = url.count('?')
+    count_features.append(i)
+
+    i = url.count('%')
+    count_features.append(i)
+
+    i = url.count('.')
+    count_features.append(i)
+
+    i = url.count('=')
+    count_features.append(i)
+
+    i = url.count('http')
+    count_features.append(i)
+
+    i = url.count('https')
+    count_features.append(i)
+
+    i = url.count('www')
+    count_features.append(i)
+
+    return count_features
+
+
+
+def extract_features(url):
+    url_features = []
+    i = hostname_length(url)
+    url_features.append(i)
+    i = url_length(url)
+    url_features.append(i)
+    i = fd_length(url)
+    url_features.append(i)
+    i = get_counts(url)
+    url_features.append(i)
+    i = digit_count(url)
+    url_features.append(i)
+    i = letter_count(url)
+    url_features.append(i)
+    i = no_of_dir(url)
+    url_features.append(i)
+    i = having_ip_address(url)
+    url_features.append(i)
+    return url_features
+
+
+
+
+def extract_features(url):
+    url_features = []
+
+    # hostname length
+    i = hostname_length(url)
+    url_features.append(i)
+
+    # path length
+    i = url_length(url)
+    url_features.append(i)
+
+    i = fd_length(url)
+    url_features.append(i)
+
+    i = get_counts(url)
+    url_features = url_features + i
+
+    i = digit_count(url)
+    url_features.append(i)
+
+    i = letter_count(url)
+    url_features.append(i)
+
+    i = no_of_dir(url)
+    url_features.append(i)
+
+    i = having_ip_address(url)
+    url_features.append(i)
+
+    return url_features
+
+def get_prediction(url, model_path):
+    model = keras.models.load_model(model_path)
+    url_features = extract_features(url)
+    prediction = model.predict([url_features])
+    i = prediction[0][0]*100
+    if i>=50:
+        print("There is ",i,"% chance, the url is malicious !")
+    else:
+        print("The url is not malicious")
+    return i
+
+def home(request):
+    return render(request, "home.html")
+
+def result(request):
+    model_path = "Malicious_URL_Prediction.h5"
+    url = request.GET.get('search','')
+    
+
+def get_prediction(url, model_path):
+    #print("Loading the model...")
+    model = keras.models.load_model(model_path)
+
+    #print("Extracting features from url...")
+    url_features = extract_features(url)
+    #print(url_features)
+
+    #print("Making prediction...")
+    prediction = model.predict([url_features])
+    i = prediction[0][0] * 100
+    i = round(i,3)
+    if i >=50 :
+        print("There is ",i,"% chance,the url is malicious !")
+        #a = "There is ",i,"% chance,the url is malicious !"
+    else:
+        print("The url is not malicious")
+        #a = "The url is not malicious"
+
+    return i
+
+def home(request):
+    return render(request, "home.html")
+
+def result(request):
+    # Load the Keras model
+    #model = keras.models.load_model('Malicious_URL_Prediction.h5')
+    model_path = "Malicious_URL_Prediction.h5"
+
+# input url
+    url = request.GET.get('search','')
+    #print("hell",url)
+    #print("hello")
+    #url = "www.google.com"    #print(url)
+
+
+    try:
+    # Get the prediction using the get_prediction function
+        prediction = get_prediction(url, model_path)
+    
+    # Print the prediction
+        print(f"Probability of URL being malicious: {prediction}")
+        b = f"Probability of URL being malicious: {prediction}"
+    except Exception as e:
+    # Handle any exceptions that may occur
+        print(f"An error occurred: {str(e)}")
+        b = f"An error occurred: {str(e)}"
+    j = prediction
+    j = round(j,3)
+    if j>=50:
+        a = f"Given url is malicious!"
+    else:
+        a = f"Not Malicious"
+    return render(request, "result.html", {'a': a, 'b': b})
+
+
+    # Get the user input (URL) from the request's GET parameters
+    user_url = request.GET.get('RI', '')  # Assuming 'RI' is the name of the input field in your form
+    result=get_prediction(user_url,model)
+    print(result)
+
+    # You can choose to print the user input or process it in any way you like here
+
+    # Comment out the prediction-related code to keep it inactive
+    # Perform feature extraction on the user URL (you may need to implement this)
+    url_features = extract_features(user_url)  # Implement extract_features as needed
+
+    # Make predictions using the model
+    #prediction = model.predict([url_features])
+
+    # Assuming 'prediction' is the probability of being malicious (0 to 1)
+    # You can convert it to a more user-friendly message or format
+    #probability = prediction[0][0] * 100
+    #probability = round(probability, 3)
+
+    # Create a context dictionary to pass data to the template
+    #context = {
+        #'user_url': user_url,
+        # 'probability': probability,
+    #}
+    
